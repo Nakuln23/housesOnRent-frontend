c49fd14bc5f314c36d0e7c78077efbbefd035435
nakuln23 <nakulnagargade@gmail.com>
2019-06-18 15:11:29 +0530

    Created frontend with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import "./assests/css/bootstrap.min.css"
+import "./assests/css/animate.css";
+//import "./assests/css/font-awesome.min.css";
+import "./assests/css/magnific-popup.css";
+import Navbar from './components/Navbar';
+// import "./assests/js/jquery-3.2.1.min.js"
+// import "./assests/css/owl.carousel.css";
+// import "./assests/js/owl.carousel.min.js";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import LandingPage from './views/landingPage/Index';
+import SearchPage from './views/searchpage';
+import HousePage from './views/housePage';
+import Footer from './components/Footer';
+import Login from './views/auth/Login';
+import Register from './views/auth/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Route exact path="/" component = {LandingPage}/>
+      <Route exact path="/search/:city" component = {SearchPage}/>
+      <Route exact path="/house/:house_id" component = {HousePage}/>
+      <Route exact path="/user/register" component = {Register}/>
+      <Route exact path="/user/login" component = {Login}/>
+      <Footer/>
+    </Router>
+    
+    </>
   );
 }
 
